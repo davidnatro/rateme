e793d4c5cae7d0f3697ed0fbf203741ca6613d9a
David Natro <davidnatro03@gmail.com>
2023-12-09 10:47:35 +0600

    Revert "Revert "ws security and vault passwords""
    This reverts commit e218470ef09331a2b6ca91b5788d9636a27d60a0.

--- src/main/java/me/rate/rateme/config/SecurityConfig.java
+++ src/main/java/me/rate/rateme/config/SecurityConfig.java
@@ -58,7 +58,9 @@ public class SecurityConfig {
                               .requestMatchers(GET, "/company/**").hasRole(USER.name())
                               .requestMatchers(POST, "/company/**").hasRole(COMPANY_HEAD.name())
                               .requestMatchers(PUT, "/company/**").hasRole(COMPANY_HEAD.name())
-                              .requestMatchers(DELETE, "/company/**").hasRole(COMPANY_HEAD.name()))
+                              .requestMatchers(DELETE, "/company/**").hasRole(COMPANY_HEAD.name())
+
+                              .requestMatchers(GET, "/room/**").permitAll())
                .httpBasic(Customizer.withDefaults()).build();
   }
 
