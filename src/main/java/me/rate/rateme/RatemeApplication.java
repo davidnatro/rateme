98714d6507b44963317c519b696c24b2ef74a871
David Natro <davidnatro03@gmail.com>
2023-12-09 10:46:15 +0600

    Revert "removed gradle"
    This reverts commit 294d429357e890a7a38ba8ee1e5a1107389fd6f2.

--- src/main/java/me/rate/rateme/RatemeApplication.java
+++ src/main/java/me/rate/rateme/RatemeApplication.java
@@ -1,6 +1,5 @@
 package me.rate.rateme;
 
-import java.math.BigInteger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
